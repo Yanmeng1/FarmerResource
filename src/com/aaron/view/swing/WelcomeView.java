5cdc8f5ca0a2a4ca902dabd40977d6df240961ab
Aaron Yan <ymeng125@126.com>
2017-11-23 16:35:03 +0800

    20171123

--- src/com/aaron/view/swing/WelcomeView.java
+++ src/com/aaron/view/swing/WelcomeView.java
@@ -10,19 +10,19 @@ import org.jb2011.lnf.beautyeye.BeautyEyeLNFHelper;
 
 public class WelcomeView {
 	public static void main(String[] args) {
-	    try
-	    {
-	    	 //设置本属性将改变窗口边框样式定义
-	        BeautyEyeLNFHelper.frameBorderStyle = BeautyEyeLNFHelper.FrameBorderStyle.generalNoTranslucencyShadow	;
-	    	// 设置不可见
-	        UIManager.put("RootPane.setupButtonVisible", false);
-	        org.jb2011.lnf.beautyeye.BeautyEyeLNFHelper.launchBeautyEyeLNF();
-	        UIManager.put("ToolBar.isPaintPlainBackground", Boolean.TRUE);
-	    }
-	    catch(Exception e)
-	    {
-	        e.printStackTrace();	    
-	    }
+//	    try
+//	    {
+//	    	 //设置本属性将改变窗口边框样式定义
+//	        BeautyEyeLNFHelper.frameBorderStyle = BeautyEyeLNFHelper.FrameBorderStyle.generalNoTranslucencyShadow	;
+//	    	// 设置不可见
+//	        UIManager.put("RootPane.setupButtonVisible", false);
+//	        org.jb2011.lnf.beautyeye.BeautyEyeLNFHelper.launchBeautyEyeLNF();
+//	        UIManager.put("ToolBar.isPaintPlainBackground", Boolean.TRUE);
+//	    }
+//	    catch(Exception e)
+//	    {
+//	        e.printStackTrace();	    
+//	    }
 		new ViewFrame(40, 40);
 	}
 }
